da4df87ef958349c63454fad3d9c777ee21dcac3
VinceRover75 <leon.goetz2@googlemail.com>
2026-02-10 01:35:22 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,7 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("healthpods-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "cap.html",
-        "tele-home.html",
-        "tele-pod.html",
-        "confirmation.html",
-        "feedback.html",
-        "css/style.css",
-        "js/app.js",
-        "assets/map.png"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
